535987da442dfed096103354a36fedbf73d33ac0
MiguVT <71216796+MiguVT@users.noreply.github.com>
2025-11-29 21:08:50 +0100

    Add script to reboot SteamVR headset fixing DRM issue (Linux)

---
+++ VR/fixvr/fixvr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Path to SteamVR tools (Standard Arch/Steam path)
+# If you installed Steam via Flatpak, paths will differ.
+STEAMVR_PATH="$HOME/.local/share/Steam/steamapps/common/SteamVR"
+CONSOLE="$STEAMVR_PATH/tools/lighthouse/bin/linux64/lighthouse_console"
+
+# Send the reboot command to the headset
+echo "reboot" | "$CONSOLE"
