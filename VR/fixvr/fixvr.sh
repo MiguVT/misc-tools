1249f8fcb93eddd6653a8191d76fed95aaca7def
MiguVT <71216796+MiguVT@users.noreply.github.com>
2025-11-29 21:35:08 +0100

    Update reboot command to include quit command

--- VR/fixvr/fixvr.sh
+++ VR/fixvr/fixvr.sh
@@ -5,4 +5,4 @@ STEAMVR_PATH="$HOME/.local/share/Steam/steamapps/common/SteamVR"
 CONSOLE="$STEAMVR_PATH/tools/lighthouse/bin/linux64/lighthouse_console"
 
 # Send the reboot command to the headset
-echo "reboot" | "$CONSOLE"
+echo -e "reboot\nquit" | "$CONSOLE"
